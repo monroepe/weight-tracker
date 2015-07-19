4b874811e6abd5eb20681ebafe68c1dbf846f8c3
Peter <monroepe@gmail.com>
2015-07-19 19:12:51 -0400

    Toggle complete button

--- client/components/habits/habits.js
+++ client/components/habits/habits.js
@@ -5,4 +5,14 @@ Template.habits.helpers({
 });
 
 Template.habits.events({
+  'click #complete-button': function() {
+    var $this = $("#complete-button");
+    $this.toggleClass('complete');
+    $this.toggleClass('incomplete');
+    if ($this.val() == "Incomplete") {
+      $this.val("Complete");
+    } else {
+      $this.val("Incomplete");
+    }
+  }
 });
